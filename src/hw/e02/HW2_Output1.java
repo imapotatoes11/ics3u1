288c2a854bd42e8d0a622efb830c273d503530c6
imapotatoes11 <wzm825@gmail.com>
2023-09-11 14:57:27 -0400

    minor changes

--- src/hw/e02/HW2_Output1.java
+++ src/hw/e02/HW2_Output1.java
@@ -4,15 +4,15 @@ public class HW2_Output1 {
     public static void main(String[] args) {
         System.out.println("Num   Square");
         System.out.println("***   ******");
-        System.out.println("1     " + Math.pow(1,1));
-        System.out.println("2     " + Math.pow(2,2));
-        System.out.println("3     " + Math.pow(3,3));
-        System.out.println("4     " + Math.pow(4,4));
-        System.out.println("5     " + Math.pow(5,5));
+        System.out.println("1     " + (int)Math.pow(1,2));
+        System.out.println("2     " + (int)Math.pow(2,2));
+        System.out.println("3     " + (int)Math.pow(3,2));
+        System.out.println("4     " + (int)Math.pow(4,2));
+        System.out.println("5     " + (int)Math.pow(5,2));
         System.out.println();
-        System.out.println("1*1 = " + Math.pow(1,1));
-        System.out.println("2*2 = " + Math.pow(2,2));
-        System.out.println("3*3 = " + Math.pow(3,3));
-        System.out.println("4*4 = " + Math.pow(4,4));
+        System.out.println("1*1 = " + (int)Math.pow(1,2));
+        System.out.println("2*2 = " + (int)Math.pow(2,2));
+        System.out.println("3*3 = " + (int)Math.pow(3,2));
+        System.out.println("4*4 = " + (int)Math.pow(4,2));
     }
 }
--- src/hw/e02/HW2_Output3.java
+++ src/hw/e02/HW2_Output3.java
@@ -3,7 +3,8 @@ package hw.e02;
 public class HW2_Output3 {
     public static void main(String[] args) {
         System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 5; i++) {
             System.out.println("\\                          \\");
+        }
     }
 }
