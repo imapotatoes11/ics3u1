0470cd74c9de92f702e1a95e7fe0b34ca86a12a2
imapotatoes11 <wzm825@gmail.com>
2023-09-12 12:59:31 -0400

    minor changes

---
+++ src/hw/e03/HW3_Output1.java
@@ -0,0 +1,12 @@
+package hw.e03;
+
+public class HW3_Output1 {
+    public static void main(String[] args) {
+        System.out.println(5);
+        System.out.println(" ".repeat(3) + "3");
+        System.out.println("5*2=" + 5*2);
+        System.out.println("My name is" + " ".repeat(8) + "George!!!!");
+        System.out.println("3.0   10.563  harry\n\n\n");
+        System.out.println("I think I got it!");
+    }
+}
