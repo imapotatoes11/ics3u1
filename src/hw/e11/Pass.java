3fd57eda3490e3624e793cde0efaa940a345039e
Kevin Wang <wzm825@gmail.com>
2023-10-05 18:18:53 -0400

    amen

--- src/hw/e11/Pass.java
+++ src/hw/e11/Pass.java
@@ -1,128 +1,227 @@
+/*
+ * Copyright 2023 Kevin Wang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the license at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the license is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * license for the specific language governing permissions and limitations under
+ * the license.
+ * */
+/*
+* Oct 05, 2023
+* The following program asks the user to solve a random arithmetic expression
+* */
 package hw.e11;
 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Stack;
+import java.util.Scanner;
 
 public class Pass {
     public static Object randChoice(Object[] a) {
+        /*
+        * Returns a random element from the given array
+         */
         Random random = new Random();
         return a[random.nextInt(a.length)];
-    }
+    } public static int randInt(int origin, int bound) {
+        /*
+        * Returns a random integer between the given bounds
+         */
+		Random random = new Random();
+		return random.nextInt(origin, bound);
+	}
     public static void main(String[] args) {
-        Expression exp = Expression.randomExpression();
-        for (Value term : exp.terms) {
-            System.out.print(term.value + " ");
+        // initialize scanner class
+        Scanner sc = new Scanner(System.in);
+        // generate random expression
+        HoldList list = HoldList.randomExpression();
+        // evaluate expression and round to nearest int
+        int result = (int) Math.round(list.evaluate());
+
+        // ask user to solve expression
+        System.out.printf("What is %s (rounded to the nearest whole number)? ", list);
+        int answer = sc.nextInt();
+
+        // confirm user's answer
+        if (answer == result) {
+            System.out.println("Correct!");
+        } else {
+            System.out.printf("Incorrect. The answer is approximately %d.\n(The real answer is %f)", result, list.evaluate());
         }
-        System.out.println();
-        System.out.println(exp.evaluate());
     }
 }
-class Expression {
-    public ArrayList<Value> terms;
-    public Expression(ArrayList<Value> terms) {
-        this.terms = terms;
+
+
+class HoldList {
+    /*
+    * A class that holds a list of "terms", essentially a class to hold an expression
+     */
+
+    // holds the list of terms
+    public ArrayList<Hold> holds;
+
+    public HoldList(ArrayList<Hold> holds) {
+        // constructor sets the expression into this.holds
+        this.holds = holds;
     }
-    public static Expression randomExpression() {
-        ArrayList<Value> terms = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
-            terms.add(new Value(Pass.randChoice(new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})));
-            terms.add(new Value(Pass.randChoice(new Object[]{"+", "-", "*", "/"})));
-        }
-        terms.add(new Value(Pass.randChoice(new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})));
-        return new Expression(terms);
+    private static double eval(ArrayList<Hold> holds) {
+        // create a StringBuilder to turn the expression into a string for Eval.eval() to evaluate
+         StringBuilder out = new StringBuilder();
+         for (Hold hold : holds) {
+             out.append(hold);
+         }
+
+         // evaluate and return result
+         return Eval.eval(out.toString());
+    }
+    public void evaluateInPlace() {
+        // evaluate the expression and set it to the first element in this.holds
+        // not used but might be useful in the future
+        Hold hold = new Hold(HoldList.eval(holds));
+        holds = new ArrayList<Hold>();
+        holds.add(hold);
     }
     public double evaluate() {
-        ArrayList<Value> postfixExpression = convertToPostfix();
-
-        // Evaluate the postfix expression using a stack
-        Stack<Double> stack = new Stack<>();
-        for (Value term : postfixExpression) {
-            if (term.isOperator()) {
-                double operand2 = stack.pop();
-                double operand1 = stack.pop();
-                double result = performOperation((char) term.value, operand1, operand2);
-                stack.push(result);
-            } else {
-//                try {
-                    stack.push((double) ((Integer) term.value).intValue());
-//                } catch (ClassCastException e) {
-//                    // TODO: gawd damn
-//                }
-            }
-        }
-
-        return stack.pop();
+        // evaluates the current expression and returns it as a double
+        return HoldList.eval(holds);
     }
-    private int getPrecedence(char operator) {
-        switch (operator) {
-            case '+':
-            case '-':
-                return 1;
-            case '*':
-            case '/':
-                return 2;
-            default:
-                return 0; // Assuming other characters are operands
-        }
+    public static double evaluate(HoldList holdList) {
+        // evaluates the given expression and returns it as a double
+        // not used but might be useful in the future
+        return HoldList.eval(holdList.toArrayList());
     }
-
-    private ArrayList<Value> convertToPostfix() {
-        ArrayList<Value> postfixExpression = new ArrayList<>();
-        Stack<Character> operatorStack = new Stack<>();
-
-        for (Value term : terms) {
-            if (term.isOperator()) {
-                char operator = (char) term.value;
-
-                while (!operatorStack.isEmpty() && getPrecedence(operatorStack.peek()) >= getPrecedence(operator)) {
-                    postfixExpression.add(new Value(operatorStack.pop()));
-                }
-
-                operatorStack.push(operator);
-            } else {
-                postfixExpression.add(term);
-            }
-        }
-
-        while (!operatorStack.isEmpty()) {
-            postfixExpression.add(new Value(operatorStack.pop()));
-        }
-
-        return postfixExpression;
+    public ArrayList<Hold> toArrayList() {
+        // returns the expression as an ArrayList
+        ArrayList<Hold> out = new ArrayList<Hold>();
+        for(Hold i : this.holds) {
+            out.add(i);
+        } return out;
     }
-    private double performOperation(char operator, double operand1, double operand2) {
-        switch (operator) {
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '*':
-                return operand1 * operand2;
-            case '/':
-                if (operand2 == 0) {
-                    throw new ArithmeticException("Division by zero");
-                }
-                return operand1 / operand2;
-            default:
-                throw new IllegalArgumentException("Invalid operator: " + operator);
+    public String toString() {
+        // returns the expression as a string
+        StringBuilder out = new StringBuilder();
+        for (Hold hold : holds) {
+            out.append(hold);
+        } return out.toString();
+    }
+    public static HoldList randomExpression() {
+        // generates a random expression with 3-30 terms
+        ArrayList<Hold> holds = new ArrayList<>();
+        for (int i = 0; i < Pass.randInt(3, 30); i++) {
+            holds.add(new Hold(Pass.randChoice(new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})));
+            holds.add(new Hold(Pass.randChoice(new Object[]{"+", "-", "*", "/"})));
         }
+        holds.add(new Hold(Pass.randChoice(new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})));
+        return new HoldList(holds);
     }
 }
-class Value {
-    public Object value;
-    public Value(Object value) {
-        if (value instanceof String)
-            try {
-                this.value = (int) Integer.parseInt((String) value);
-            } catch (NumberFormatException e) {
-                this.value = value;
-            }
-        else
-            this.value = value;
-    }
 
-    public boolean isOperator () {
-        return value instanceof Character && ("+-*/").indexOf((char) value) != -1;
+
+class Hold {
+    // object for holding a value (number or operator)
+    public Object value;
+    public Hold(Object value) {
+        this.value = value;
+    }
+    public String toString() {
+        // wow i sure wonder what this method does
+        return value.toString();
+    }
+}
+
+class Eval{
+    // eval() method from Boann
+    // https://stackoverflow.com/a/26227947
+    public static double eval(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                return x;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)` | number
+            //        | functionName `(` expression `)` | functionName factor
+            //        | factor `^` factor
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if      (eat('+')) x += parseTerm(); // addition
+                    else if (eat('-')) x -= parseTerm(); // subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if      (eat('*')) x *= parseFactor(); // multiplication
+                    else if (eat('/')) x /= parseFactor(); // division
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return +parseFactor(); // unary plus
+                if (eat('-')) return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    if (!eat(')')) throw new RuntimeException("Missing ')'");
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') { // functions
+                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    if (eat('(')) {
+                        x = parseExpression();
+                        if (!eat(')')) throw new RuntimeException("Missing ')' after argument to " + func);
+                    } else {
+                        x = parseFactor();
+                    }
+                    if (func.equals("sqrt")) x = Math.sqrt(x);
+                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                    else throw new RuntimeException("Unknown function: " + func);
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char)ch);
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
     }
 }
